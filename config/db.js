ca2648464e77986aecce4ffb16e0792f2e96e8ef
Gabriel Ekodo <ekodogabriel@outlook.com>
2022-11-27 15:39:13 +0300

    backend initial commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,16 @@ module.exports = {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USERNAME,
   PASSWORD: process.env.DB_PASSWORD,
-  DB: "sequelize_database_development",
+  DB: "d35q7k0m177qke",
   dialect: "postgres",
-  // url: `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/sequelize_database_development`,
-  url: "postgres://yuvofvhbntkonu:0bebdf6ee10a2fc5f40240274947f0890cb9c6bb06d6c7275a92f09c5ac8cebb@ec2-52-1-17-228.compute-1.amazonaws.com:5432/d35q7k0m177qke",
+  dialectOptions: {
+    ssl: {
+      require: false, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
+  url: process.env.DATABASE_URL,
+
   pool: {
     max: 5,
     min: 0,
